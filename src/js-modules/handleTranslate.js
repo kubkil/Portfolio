db0ac55aa29a1231fedbbadcd977ffd449348c3c
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-26 11:43:06 +0100

    Add media query
    Media query controls if menu gets scaled and translated on a href click.

--- src/js-modules/handleTranslate.js
+++ src/js-modules/handleTranslate.js
@@ -12,7 +12,21 @@ const handleTranslate = (() => {
     tl.to(pieMenu, 1, { x: xTo, y: yTo }, 0);
   };
 
+  const widthChange = mediaQ => {
+    if (mediaQ.matches) {
   pieMenu.addEventListener('click', translate);
+      console.log('listening');
+    } else {
+      pieMenu.removeEventListener('click', translate);
+      console.log('not listening');
+    }
+  };
+
+  if (matchMedia) {
+    const mq = window.matchMedia('(min-width: 1280px');
+    mq.addListener(widthChange);
+    widthChange(mq);
+  }
 })();
 
 export default handleTranslate;
