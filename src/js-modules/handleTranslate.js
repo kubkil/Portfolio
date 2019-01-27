84964ddac3200a2ed50bbadd47f2851098121e17
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-27 13:25:39 +0100

    Change name of selected class

--- src/js-modules/handleTranslate.js
+++ src/js-modules/handleTranslate.js
@@ -1,10 +1,7 @@
 import { TimelineLite } from 'gsap/TweenMax';
 
 const handleTranslate = (() => {
-  const sectionMenu = document.querySelector('.home');
-  const pieMenu = document.querySelector('.home__menu');
-  const xTo = -(0.45 * sectionMenu.clientWidth);
-  const yTo = -(0.4 * sectionMenu.clientHeight);
+  const pieMenu = document.querySelector('.nav__menu');
   const tl = new TimelineLite();
 
   const translate = () => {
