facbc886c53f50c2e9e79fe36bc54b8ea8c3d9fa
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-27 16:55:21 +0100

    Hide licenseKey in env variable

--- src/js-modules/handleFullpage.js
+++ src/js-modules/handleFullpage.js
@@ -2,7 +2,7 @@ import Fullpage from 'fullpage.js';
 
 const handleFullpage = (() => {
   const myFullpage = new Fullpage('#fullpage', {
-    licenseKey: '34AAD86E-133F4E86-B264FE61-AE7318D7',
+    licenseKey: process.env.LICENSE,
     responsiveWidth: 1280,
     verticalCentered: false
   });
