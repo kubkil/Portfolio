808ad22ed3f1095b2bec3bfd9dc3f7c7aac6c2fb
Jakub Kilar <kuba.kilar@gmail.com>
2019-01-27 17:52:07 +0100

    Unimport handleSmoothScroll
    Obsolete due to fullpage.js.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import style from './scss/main.scss';
 import handleRotate from './js-modules/handleRotate';
 import handleScale from './js-modules/handleScale';
 import handleTranslate from './js-modules/handleTranslate';
-import handleSmoothScroll from './js-modules/handleSmoothScroll';
 import handleFullpage from './js-modules/handleFullpage';
