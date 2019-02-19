215c140664acb8fbf07844c278165810f4cbf0a9
Jakub Kilar <kuba.kilar@gmail.com>
2019-02-19 20:53:30 +0100

    Add fetch
    Add function that posts form data to server.

--- src/js-modules/handleValidation.js
+++ src/js-modules/handleValidation.js
@@ -2,9 +2,22 @@ const handleValidation = (() => {
   const validationInput = document.querySelector('#validation');
   const form = document.querySelector('.contact__form');
 
-  form.addEventListener('submit', e => {
-    if (!(validationInput.value === '6')) e.preventDefault();
-  });
+  const sendForm = event => {
+    event.preventDefault();
+    if (validationInput.value === '15') {
+      const data = new FormData(form);
+      const url = form.getAttribute('action');
+      const method = form.getAttribute('method');
+
+      fetch(url, {
+        method: method.toUpperCase(),
+        body: data
+      })
+        .then(res => res.json())
+        .catch(err => console.log(err));
+    }
+  };
+
 })();
 
 export default handleValidation;
