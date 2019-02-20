e7153ebd68e67a138bed0f56ece157f16987736c
Jakub Kilar <kuba.kilar@gmail.com>
2019-02-20 22:23:15 +0100

    Convert formdata to json

--- src/js-modules/handleFormPost.js
+++ src/js-modules/handleFormPost.js
@@ -6,8 +6,13 @@ const handleValidation = (() => {
     event.preventDefault();
     if (validationInput.value === '15') {
       const data = new FormData(form);
+      let dataJSON = {};
+      for (const [key, value] of data) {
+        dataJSON[key] = value;
+      }
       const url = form.getAttribute('action');
       const method = form.getAttribute('method');
+      console.log(dataJSON);
 
       fetch(url, {
         method: method.toUpperCase(),
