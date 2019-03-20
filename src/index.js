6b8fff3046226eed27bcc5faa49c1131bc807745
Jakub Kilar <kuba.kilar@gmail.com>
2019-03-20 21:34:47 +0100

    Import handleValdiation

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import handleScale from './js-modules/handleScale';
 import handleFullpage from './js-modules/handleFullpage';
 import handleRotate from './js-modules/handleRotate';
 import handleTranslate from './js-modules/handleTranslate';
+import handleValidation from './js-modules/handleValidation';
 import handleFormPost from './js-modules/handleFormPost';
